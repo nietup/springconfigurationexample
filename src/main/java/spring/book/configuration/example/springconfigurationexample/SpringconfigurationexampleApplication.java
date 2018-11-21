b42ae2c5bfa8f233b1a3ea6bddcbb00f788b200c
Jakub Nietupski <jnie@netcompany.com>
2018-11-21 11:47:17 +0100

    initial commit

---
+++ src/main/java/spring/book/configuration/example/springconfigurationexample/SpringconfigurationexampleApplication.java
@@ -0,0 +1,12 @@
+package spring.book.configuration.example.springconfigurationexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringconfigurationexampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringconfigurationexampleApplication.class, args);
+    }
+}
---
+++ src/test/java/spring/book/configuration/example/springconfigurationexample/SpringconfigurationexampleApplicationTests.java
@@ -0,0 +1,16 @@
+package spring.book.configuration.example.springconfigurationexample;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringconfigurationexampleApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
